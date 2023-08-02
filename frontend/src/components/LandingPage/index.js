b537e681ad08e5316df272c0f998992655e35ee9
mikejhouseman <mikej.houseman@gmail.com>
2023-08-01 19:11:43 -0700

    Edits to Landing Page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,4 +1,4 @@
-// frontend/src/components/HomePage/index.js
+// frontend/src/components/LandingPage/index.js
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -7,8 +7,6 @@ import './LandingPage.css';
 import Navigation from '../Navigation';
 import ProfileButton from '../Navigation/ProfileButton';
 import AddSpotModal from '../AddSpotModal';
-
-
 
 const LandingPage = () => {
   const dispatch = useDispatch();
@@ -21,30 +19,43 @@ const LandingPage = () => {
   }, [dispatch]);
 
   const toggleAddSpotModal = () => {
-    setShowAddSpotModal((prevState) => !prevState);
+    setShowAddSpotModal(prevState => !prevState);
+  };
+
+  // Function to get the star rating or display "New" if no reviews
+  const getStarRating = spot => {
+    const avgRating = spot.avgStarRating;
+    return avgRating !== undefined ? avgRating.toFixed(1) : 'New';
   };
 
   return (
-    <div className="home-page">
+    <div className="landing-page">
       <Navigation isLoaded={true} />
-      <h1 className="home-page__title">Welcome to Homewrecker</h1>
-      <div className="home-page__content">
-        <div className="home-page__content__spots">
+      <h1 className="landing-page__title">Welcome to Homewrecker</h1>
+      <div className="landing-page__content">
+        <div className="landing-page__content__spots">
           <h2>Spots</h2>
           <ul className="spot-list">
-            {spots?.map((spot, i) => (
+            {spots?.map(spot => (
               <li key={spot.id} className="spot-tile">
                 <Link to={`/spots/${spot.id}`} className="spot-link">
                   <img src={spot.previewImage} alt={spot.previewImage} className="spot-image" />
-                  <span className="spot-name">{spot.name}</span>
-                  <span className="spot-location">{spot.city}, {spot.state}, ${spot.price}, {spot.avgStarRating}</span>
+                  <span className="spot-location">
+                    {spot.city}, {spot.state}, ${spot.price} night
+                  </span>
+                  <span className="spot-rating">
+                    Rating: {getStarRating(spot)}
+                    {spot.avgStarRating !== undefined && (
+                      <span className="spot-tooltip">{spot.name}</span>
+                    )}
+                  </span>
                 </Link>
               </li>
             ))}
           </ul>
           <button onClick={toggleAddSpotModal}>Add Spot</button>
-        {showAddSpotModal && <AddSpotModal />}
-      </div>
+          {showAddSpotModal && <AddSpotModal />}
+        </div>
       </div>
       {sessionUser && <ProfileButton key={sessionUser.id} />}
     </div>
